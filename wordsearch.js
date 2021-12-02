c695c1622448370233dee9ae9919dc1b2117b7e5
Julien <flynstone@live.ca>
2021-12-01 22:18:12 -0500

    commit pair-programming word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (array) {
+
+  let results = [];
+
+  for (let i = 0; i < array[0].length; i++) {
+    let row = [];
+    for (let j = 0; j < array.length; j++) {
+      row.push(array[j][i]);
     }
-}
+    results.push(row);
+  }
+  return results;
+};
+
+
+const wordSearch = (letters, word) => {
+
+  if (letters.length < 1 || word.length < 1) return false;
+
+  const horizontalJoin = letters.map(ls => ls.join(''))
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true
+    }
+  };
+
+  const vertical = transpose(letters)
+  const verticalJoin = vertical.map(ls => ls.join(''))
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
 
 module.exports = wordSearch
+
